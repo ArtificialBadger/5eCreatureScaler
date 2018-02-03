b73baeb657e32e70f1d3b95847f67de3c8fd6102
Isaac Zais <isaaczais@hotmail.com>
2018-02-02 21:17:42 -0600

    Added Languages

---
+++ 5eCreatureScaler/Models/Languages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreatureScaler.Models
+{
+    public enum Languages
+    {
+        Common,
+        Dwarvish,
+        Elvish,
+        Giant,
+        Gnomish,
+        Goblin,
+        Halfling,
+        Abussal,
+        Celestial,
+        Draconic,
+        DeepSpeech,
+        Infernal,
+        Sylvan,
+        Undercommon,
+        Primordial,
+        Aquan,
+        Auran,
+        Ignan,
+        Terran,
+    }
+}
