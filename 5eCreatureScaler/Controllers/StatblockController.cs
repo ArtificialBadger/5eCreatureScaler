3d0e8276b98accfe26646cbb07be3279f26c525d
Isaac Zais <isaaczais@hotmail.com>
2018-02-09 14:24:38 -0600

    Bound more fields to the Razor Page

--- 5eCreatureScaler/Controllers/StatblockController.cs
+++ 5eCreatureScaler/Controllers/StatblockController.cs
@@ -16,10 +16,16 @@ namespace CreatureScaler.Controllers
 
         public IActionResult Statblock()
         {
-            var creature = Creature.Create("Crazy Train", Size.Medium, ChallengeRating.Create(5), 15, AbilityScore.CreateStandard(16, 9, 13, 1, 3, 25), 10);
+            var creature = Creature.Create("Thomas the Tank Engine", Size.Medium, ChallengeRating.Create(5), 15, AbilityScore.CreateStandard(16, 9, 13, 1, 3, 25), 10);
 
             creature.DamageImmunities = new List<DamageType>() {DamageType.Fire, DamageType.Lightning, DamageType.Acid};
             creature.ConditionImmunities = new List<Condition>() {Condition.Blinded, Condition.Deafened, Condition.Prone};
+
+            creature.Actions.Add(new CreatureScaler.Models.Action() {Name="Choo Choo", Description="Melee Weapon Attack: +4 to hit, reach 5 ft., one target. Hit:5 (1d6 +2) bludgeoning damage"} );
+            creature.Actions.Add(new CreatureScaler.Models.Action() {Name="Steam Whistle", Recharge="Recharge 5-6", Description="Thomas blows hot steam in a 60-foot sphere centered on Thomas. Each creature in that area other than Thomas must make a DC 18 Dexterity saving throw, taking 35 (10d6) fire damage on a failed save, or half as much damage on a successful one."} );
+
+            creature.Features.Add(new Feature() { Name="Antimagic Susceptibility", Description="Thomas is incapacitated while in the area of an anitmagic-field. If targeted by dispel magic, Thomas must suceed on a Constitution saving throw agains that caster's spell save DC or fall unconscious for 1 minute" });
+            creature.Features.Add(new Feature() { Name="False Appearance", Description="While Thomas remains motionless, it is indistinguishable from a normal tank engine." });
 
             return View("StatBlockView", creature);
         }
